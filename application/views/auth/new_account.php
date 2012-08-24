ff74601b7e0dc5fb752f0b6055d22b7d93ea7f8c
Kieran Foxley-Jones <KieranF-J@blueyonder.co.uk>
2012-08-24 14:27:31 +0100

    New account implemented
    Remember to check config files before spending hours trying to fix a
    problem!

--- application/views/auth/new_account.php
+++ application/views/auth/new_account.php
@@ -13,33 +13,29 @@ $this->load->view('includes/navbar');
 	
 	<div id="infoMessage"><?php echo $message;?></div>
 	
-    <?php echo form_open("auth/make_account");?>
-      <p>First Name:<br />
-      <?php echo form_input($first_name);?>
-      </p>
-      
-      <p>Last Name:<br />
-      <?php echo form_input($last_name);?>
-      </p>
-      
-      <p>Company Name:<br />
-      <?php echo form_input($company);?>
-      </p>
-      
+    <?php echo form_open("auth/new_account");?>
+      <p>Username:<br />
+      <?php echo form_input($username);?>
+      </p>      
+
       <p>Email:<br />
       <?php echo form_input($email);?>
-      </p>
-      
-      <p>Phone:<br />
-      <?php echo form_input($phone1);?>-<?php echo form_input($phone2);?>-<?php echo form_input($phone3);?>
-      </p>
-      
+      </p>      
+
       <p>Password:<br />
       <?php echo form_input($password);?>
       </p>
       
       <p>Confirm Password:<br />
       <?php echo form_input($password_confirm);?>
+      </p>
+      
+      <p>API Key</br>
+      <?php echo form_input($api_key);?>
+      </p>
+      
+      <p>Verification Code</br>
+      <?php echo form_input($verification);?>
       </p>
       
       
